62f7bfe41eae07ba26885b2f17dcce9f309aa5e8
rtsai <rtsai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-19 23:34:07 +0000

    - Pass wp_list_authors args as array instead of query string. - Fix inverted control UI.
    git-svn-id: https://plugins.svn.wordpress.org/wp-authors/trunk@17162 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-authors.php
+++ wp-authors.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.tsaiberspace.net/projects/wordpress/wp-authors/
 Description: Sidebar widget to list all authors of a blog. Navigate to <a href="widgets.php">Presentation &rarr; Widgets</a> to add to your sidebar.
 Author: Robert Tsai
 Author URI: http://www.tsaiberspace.net/
-Version: 1.0.1
+Version: 1.0.2
 */
 
 function widget_wpauthors_init() {
@@ -16,13 +16,21 @@ function widget_wpauthors_init() {
 		extract($args);
 
 		$options = get_option('widget_authors');
-		$c = $options['count'] ? '1' : '0';
-		$f = $options['show_fullname'] ? '1' : '0';
-		$hide = $options['hide_empty'] ? '1' : '0';
-		$excludeadmin = $options['exclude_admin'] ? '1' : '0';
-		$title = empty($options['title']) ? __('Authors') : $options['title'];
+		$c = $options['count'] ? true : false;
+		$f = $options['show_fullname'] ? true : false;
+		$hide = $options['hide_empty'] ? true : false;
+		$excludeadmin = $options['exclude_admin'] ? true : false;
+		$title = empty($options['title']) ? __('Authors') :
+			$options['title'];
 
-		$author_args = "orderby=name&optioncount={$c}&show_fullname={$f}&hide_empty={$hide}&exclude_admin={$excludeadmin}&post_types=post";
+		$author_args = array(
+			'orderby' => 'name',
+			'optioncount' => $c,
+			'show_fullname' => $f,
+			'hide_empty' => $hide,
+			'exclude_admin' => $excludeadmin,
+			'post_types' => 'post',
+			);
 
 		print <<<EOM
 		$before_widget
@@ -30,7 +38,7 @@ function widget_wpauthors_init() {
 		$before_title$title$after_title
 EOM;
 
-		wp_list_authors($author_args . '&title_li=');
+		wp_list_authors($author_args);
 
 		print <<<EOM
 		</ul>
@@ -39,38 +47,35 @@ EOM;
 	}
 
 	function wp_widget_authors_control() {
-		$options = $newoptions = get_option('widget_authors');
-		if ( !is_array($options) )
-			$options = array(
-				'title' => 'Authors',
-				'count' => true,
-				'show_fullname' => false,
-				'hide_empty' => true,
-				'exclude_admin' => true,
-				);
+		$defaults = array(
+			'title' => 'Authors',
+			'count' => true,
+			'show_fullname' => false,
+			'hide_empty' => true,
+			'exclude_admin' => true,
+			);
+		$options = get_option('widget_authors');
+		$options = array_merge($defaults, $options);
 		if ( $_POST['authors-submit'] ) {
-			$newoptions['title'] = strip_tags(stripslashes($_POST['authors-title']));
-			$newoptions['count'] = isset($_POST['authors-count']);
-			$newoptions['show_fullname'] = isset($_POST['authors-show_fullname']);
-			$newoptions['hide_empty'] = isset($_POST['authors-hide_empty']);
-			$newoptions['exclude_admin'] = isset($_POST['authors-exclude_admin']);
-		}
-		if ( $options != $newoptions ) {
-			$options = $newoptions;
+			$options['title'] = strip_tags(stripslashes($_POST['authors-title']));
+			$options['count'] = isset($_POST['authors-count']);
+			$options['show_fullname'] = isset($_POST['authors-show_fullname']);
+			$options['hide_empty'] = isset($_POST['authors-hide_empty']);
+			$options['exclude_admin'] = isset($_POST['authors-exclude_admin']);
 			update_option('widget_authors', $options);
 		}
 		$title = attribute_escape($options['title']);
 		$count = $options['count'] ? 'checked="checked"' : '';
 		$show_fullname = $options['show_fullname'] ? 'checked="checked"' : '';
-		$show_empty = !$options['hide_empty'] ? 'checked="checked"' : '';
-		$show_admin = !$options['exclude_admin'] ? 'checked="checked"' : '';
+		$hide_empty = $options['hide_empty'] ? 'checked="checked"' : '';
+		$exclude_admin = $options['exclude_admin'] ? 'checked="checked"' : '';
 ?>
 
 						<p><label for="authors-title"><?php _e('Title:'); ?> <input style="width: 250px;" id="authors-title" name="authors-title" type="text" value="<?php echo $title; ?>" /></label></p>
 						<p style="text-align:right;margin-right:40px;"><label for="authors-count"><?php _e('Show post counts'); ?> <input class="checkbox" type="checkbox" <?php echo $count; ?> id="authors-count" name="authors-count" /></label></p>
 						<p style="text-align:right;margin-right:40px;"><label for="authors-show_fullname"><?php _e('Show full names'); ?> <input class="checkbox" type="checkbox" <?php echo $show_fullname; ?> id="authors-show_fullname" name="authors-show_fullname" /></label></p>
-						<p style="text-align:right;margin-right:40px;"><label for="authors-hide_empty"><?php _e('Hide empty authors'); ?> <input class="checkbox" type="checkbox" <?php echo $show_empty; ?> id="authors-hide_empty" name="authors-hide_empty" /></label></p>
-						<p style="text-align:right;margin-right:40px;"><label for="authors-exclude_admin"><?php _e('Exclude admin'); ?> <input class="checkbox" type="checkbox" <?php echo $show_admin; ?> id="authors-exclude_admin" name="authors-exclude_admin" /></label></p>
+						<p style="text-align:right;margin-right:40px;"><label for="authors-hide_empty"><?php _e('Hide empty authors'); ?> <input class="checkbox" type="checkbox" <?php echo $hide_empty; ?> id="authors-hide_empty" name="authors-hide_empty" /></label></p>
+						<p style="text-align:right;margin-right:40px;"><label for="authors-exclude_admin"><?php _e('Exclude admin'); ?> <input class="checkbox" type="checkbox" <?php echo $exclude_admin; ?> id="authors-exclude_admin" name="authors-exclude_admin" /></label></p>
 						<input type="hidden" id="authors-submit" name="authors-submit" value="1" />
 <?php
 	}
